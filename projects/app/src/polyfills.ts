e06a466219543b2809f11ee83e59ddb71639b57e
Leandro Carlos Pereira <lcpereira.dev@gmail.com>
2020-12-18 11:47:46 -0300

    fix(project-ios): adds polyfills in attempt to fix error in IOS

--- projects/app/src/polyfills.ts
+++ projects/app/src/polyfills.ts
@@ -19,12 +19,10 @@
  */
 
 /** IE9, IE10 and IE11 requires all of the following polyfills. */
-import 'core-js/es6';
-import 'core-js/es7';
+import 'core-js/es';
 
 /** Evergreen browsers require these. */
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+import 'core-js/es/reflect';
 
 /** IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
