ce98493215b1393c651c04fc5a8478ae85d5b2f3
Leandro Carlos Pereira <lcpereira.dev@gmail.com>
2021-03-09 10:38:55 -0300

    fix(test-deploys): configurate files to publish lib

--- npm_publish.sh
+++ npm_publish.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}">.npmrc
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}">~/.npmrc
+cat ~/.npmrc
 cd dist/ngx-speech-recognition
 npm publish --access=public
