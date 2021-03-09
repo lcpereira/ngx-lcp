9d444a4a98818fc752ff9410a1c4c80d403179e1
Leandro Carlos Pereira <lcpereira.dev@gmail.com>
2021-03-09 09:37:10 -0300

    fix(test-deploys): configurate files to publish lib

--- npm_publish.sh
+++ npm_publish.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo $NPM_EMAIL
+echo "############# 1"
+echo ${NPM_EMAIL}
+echo "############# 2"
 echo ${TRAVIS_REPO_SLUG} | cut -f 1 -d /
-
+echo "############# 3"
 cd dist/ngx-speech-recognition
 ls
