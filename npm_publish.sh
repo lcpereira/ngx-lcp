eb116d51730b707b8d1e6742a0ab95279b461abf
Leandro Carlos Pereira <lcpereira.dev@gmail.com>
2021-03-11 21:47:40 -0300

    fix(deploy): fix not to publish package with existing version

--- npm_publish.sh
+++ npm_publish.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}">~/.npmrc
-cat ~/.npmrc
 
 paths=('speech-recognition' 'upload-file')
 
 npm_version=$(npm show @ngx-lcp/${paths[0]} version)
-echo $npm_version
+package_version=$(node -p -e "require('./package.json').version")
 
-PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
-echo $PACKAGE_VERSION
-
-if [ $npm_version != $PACKAGE_VERSION ]; then
-  for path in ${paths[@]}; do
-    cd dist/ngx-$path
-    npm publish --access=public
-    cd ../../
-  done
+if [ $npm_version != $package_version ];
+  then
+    for path in ${paths[@]}; do
+      cd dist/ngx-$path
+      npm publish --access=public
+      cd ../../
+    done
+  else
+    echo 'Unpublished packages because they are in the same version'
 fi
